05b79023f7064eaf880071c2a91e3ad03bf99972
aristarrtled <haven.barnes26@gmail.com>
2024-10-13 15:55:10 -0400

    Added QuickStart file and completed  'Creating and Nesting Components'

---
+++ QuickStart/QuickStart.js
@@ -0,0 +1,15 @@
+//Creating and Nesting Components
+function MyButton(){
+    return (
+        <button>I'm a button.</button>
+    );
+}
+
+export default function MyApp(){
+    return (
+        <div>
+            <h1>Welcome to my app.</h1>
+            <MyButton />
+        </div>
+    );
+}
