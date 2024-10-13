09cfc0415dc33cbc37832ff500d1e0130a0b7ef3
aristarrtled <haven.barnes26@gmail.com>
2024-10-13 16:03:47 -0400

    Displaying Data section complete

--- QuickStart/QuickStart.js
+++ QuickStart/QuickStart.js
@@ -28,3 +28,26 @@ function AboutPage(){
 <img className="avatar" />
 //added CSS file
 
+//Displaying Data
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize:90,
+};
+
+export default function Profile(){
+    return(
+        <>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                }}
+            />
+        </>
+    );
+}
