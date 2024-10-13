114522ff21d75c6e83d506abd54205c578747f18
aristarrtled <haven.barnes26@gmail.com>
2024-10-13 17:53:22 -0400

    Started work on Remix React project, following a tutorial

---
+++ RemixProject/app/routes/jokes.$jokeId.tsx
@@ -0,0 +1,12 @@
+export default function JokeIDRoute(){
+    return(
+        <div>
+            <p>Here is your joke:</p>
+            <p>
+                What do you call a new building that
+                optomotrists want to construct?
+                A site for sore eyes.
+            </p>
+        </div>
+    );
+}
---
+++ RemixProject/app/routes/jokes._index.tsx
@@ -0,0 +1,8 @@
+export default function JokesIndexRoute (){
+    return (
+        <div>
+            <h1>What do you call a dyslexic, agnostic, insomniac?</h1>
+            <p>A guy who stayed up all night wondering if there was a dog.</p>
+        </div>
+    );
+}
